5bc092370d88df5b48534c3e739d25ec687acdac
Crhistian Fernández Leal <131958189+lealinh0@users.noreply.github.com>
2023-05-18 13:03:08 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,34 +1,57 @@
-const fs = require('fs');
-
 function leerArchivoXML(archivo) {
-  fs.readFile(archivo, 'utf-8', (err, data) => {
-    if (err) {
-      console.error('Error al leer el archivo:', err);
-      return;
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(this.responseText, 'text/xml');
+
+      const usuarios = xmlDoc.getElementsByTagName('usuario');
+
+      for (let i = 0; i < usuarios.length; i++) {
+        const usuario = usuarios[i];
+        const nombre = usuario.getAttribute('nombre');
+        const steamId = usuario.getAttribute('steam_id');
+        const estadisticas = usuario.getElementsByTagName('estadisticas')[0].attributes;
+        const rango = usuario.getElementsByTagName('rango')[0].attributes;
+        const armas = usuario.getElementsByTagName('arma');
+
+        console.log('Nombre:', nombre);
+        console.log('Steam ID:', steamId);
+        console.log('Estadísticas:', obtenerAtributos(estadisticas));
+        console.log('Rango:', obtenerAtributos(rango));
+        console.log('Armas:', obtenerArmas(armas));
+        console.log('-----------------------------');
+      }
     }
+  };
+  xhttp.open('GET', archivo, true);
+  xhttp.send();
+}
 
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(data, 'text/xml');
+function obtenerAtributos(nodo) {
+  const atributos = {};
 
-    const usuarios = xmlDoc.getElementsByTagName('usuario');
-    
-    for (let i = 0; i < usuarios.length; i++) {
-      const usuario = usuarios[i];
-      const nombre = usuario.getAttribute('nombre');
-      const steamId = usuario.getAttribute('steam_id');
-      const estadisticas = usuario.getElementsByTagName('estadisticas')[0].attributes;
-      const rango = usuario.getElementsByTagName('rango')[0].attributes;
-      const armas = usuario.getElementsByTagName('arma');
+  for (let i = 0; i < nodo.length; i++) {
+    const atributo = nodo[i];
+    atributos[atributo.name] = atributo.value;
+  }
 
-      console.log('Nombre:', nombre);
-      console.log('Steam ID:', steamId);
-      console.log('Estadísticas:', estadisticas);
-      console.log('Rango:', rango);
-      console.log('Armas:', armas);
-      console.log('-----------------------------');
-    }
-  });
+  return atributos;
+}
+
+function obtenerArmas(nodos) {
+  const armas = [];
+
+  for (let i = 0; i < nodos.length; i++) {
+    const arma = nodos[i];
+    const nombre = arma.getAttribute('nombre');
+    const atributos = obtenerAtributos(arma.attributes);
+    armas.push({ nombre, ...atributos });
+  }
+
+  return armas;
 }
 
 // Uso de la función
-leerArchivoXML('../proyecto.xml');
+leerArchivoXML('./proyecto.xml');
+

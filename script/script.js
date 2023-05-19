ed8251e5f9067b4a149f18ea1ac93afc48962154
Crhistian Fernández Leal <131958189+lealinh0@users.noreply.github.com>
2023-05-19 09:24:07 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,25 +1,20 @@
 function ejecutar(){
 
-
-  // Solicitar al usuario el ID del jugador
   const playerId = prompt("Ingresa el ID del jugador:");
   
-  // URL del archivo XML
   const xmlUrl = "https://lealinh0.github.io/counterstrike/proyecto.xml";
   
-  // Cargar el archivo XML utilizando fetch
   fetch(xmlUrl)
     .then(response => response.text())
     .then(xmlString => {
-      // Crear un objeto DOMParser
       const parser = new DOMParser();
       const xmlDoc = parser.parseFromString(xmlString, "text/xml");
   
-      // Encontrar el jugador con el ID proporcionado
+
       const jugador = xmlDoc.querySelector(`usuario[steam_id="${playerId}"]`);
   
       if (jugador) {
-        // Obtener la información del jugador
+
         const nombre = jugador.getAttribute("nombre");
         const estadisticas = jugador.querySelector("estadisticas");
         const partidasJugadas = estadisticas.getAttribute("partidas_jugadas");
@@ -27,13 +22,21 @@ function ejecutar(){
         const asesinatos = estadisticas.getAttribute("asesinatos");
         const muertes = estadisticas.getAttribute("muertes");
   
-        // Mostrar la información del jugador
+
         console.log("Información del jugador:");
         console.log(`Nombre: ${nombre}`);
         console.log(`Partidas jugadas: ${partidasJugadas}`);
         console.log(`Partidas ganadas: ${partidasGanadas}`);
         console.log(`Asesinatos: ${asesinatos}`);
         console.log(`Muertes: ${muertes}`);
+
+        alert("Información del jugador:"
+        `Nombre: ${nombre}`
+        `Partidas jugadas: ${partidasJugadas}`
+        `Partidas ganadas: ${partidasGanadas}`
+        `Asesinatos: ${asesinatos}`
+        `Muertes: ${muertes}`
+        );
       } else {
         console.log("Jugador no encontrado.");
       }
